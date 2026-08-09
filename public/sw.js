590f1e4b3d4da69f0bf5b3dd947f78284b538f4c
soloversegame <soloversegame@gmail.com>
2026-08-09 15:43:26 +0930

    added personal player mats for printing out

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'soloverse-v1';
+const ASSETS = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
