6e29cddd171cfd50567c1cb0a7f0fb02a30d9e55
soloversegame <soloversegame@gmail.com>
2026-09-13 01:48:00 +1000

    added architect's expansion further setup & added a.g.'s name to crazy 8's definitions rulebook contents list 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 3000000
+        maximumFileSizeToCacheInBytes: 3000000,
         cleanupOutdatedCaches: true,
         skipWaiting: true,
         clientsClaim: true,
